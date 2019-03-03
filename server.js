51a7b8e449aa6dd333c8b9a0d0df74c959cbdd9e
Chelsea Pierre <cpier114@fiu.edu>
2019-03-03 13:37:42 -0500

    Functional logic and session management

--- server.js
+++ server.js
@@ -65,19 +65,16 @@ app.get('/login', function (request, response) {
 });
 
 app.get('/logout', function (request, response) {
+	response.set('Content-Type', 'text/html');
 	setCookie(request, response, "; expires=Thu, 01 Jan 1970 00:00:00 GMT")
-	response.end('<h2>Logging out...</h2><meta http-equiv="refresh" content="1; url=/">')
-});
-app.get('/register', function (request, response) {
-	// TODO: Implement this
-	response.sendFile(path.join(__dirname, 'html/register.html'));
+	response.end('<h2>Logging out...</h2><meta http-equiv="refresh" content="0; url=/">')
 });
 app.get('/chat', async function (request, response) {
 	response.set('Content-Type', 'text/html');
 	// Check if the user has a cookie session, redirect to login if not
 	if (getCookie(request) == null) {
 		response.end('<h2>Session expired (or invalid), redirecting to login...</h2>' +
-			'<meta http-equiv="refresh" content="3; url=/login">')
+			'<meta http-equiv="refresh" content="2; url=/login">')
 	} else { // If so, check if the database has this session cookie anywhere and login with that user
 		try {
 			const dbPromise = await Promise.resolve()
@@ -93,7 +90,7 @@ app.get('/chat', async function (request, response) {
 						// No dice, expire the cookie and send them back to login
 						setCookie(request, response, "; expires=Thu, 01 Jan 1970 00:00:00 GMT")
 						response.end('<h2>Session expired, redirecting to login...</h2>' +
-							'<meta http-equiv="refresh" content="3; url=/login">')
+							'<meta http-equiv="refresh" content="0; url=/login">')
 					} else {
 						// Hey, a valid cookie! Give them the chat.html page
 						response.sendFile(path.join(__dirname, 'html/chat.html'));
@@ -113,6 +110,96 @@ app.get('/chat', async function (request, response) {
 app.get('/debugchat', function (request, response) {
 	response.sendFile(path.join(__dirname, 'html/chat.html'));
 });
+app.get('/register', function (request, response) {
+	response.set('Content-Type', 'text/html');
+	console.log("Cookie status: ", getCookie(request));
+
+	if (getCookie(request) != undefined && getCookie(request) != null) {
+		// The user is going to the /register page but has a sessiontoken cookie
+		// Redirect them to chat. If token is invalid, the cookie will get cleared
+		response.end('<h2>Session active, redirecting to chat...</h2>' +
+			'<meta http-equiv="refresh" content="0; url=/chat">')
+	} else {
+		// No cookie? Give them the registration form
+		response.sendFile(path.join(__dirname, 'html/register.html'));
+	}
+});
+// Routing the url at path '/login' when user submits a form
+app.post('/register', async function (request, response) {
+	response.set('Content-Type', 'text/html');
+	// response.sendFile(path.join(__dirname, 'html/login.html'));
+	request.postdata = null;
+	queryData = "";
+	request.on('data', async (data) => {
+		queryData += data
+		if (queryData.length > 1e6) {
+			queryData = ""
+			request.connection.destroy()
+		}
+		request.postdata = qs.parse(queryData)
+
+		console.log(request.postdata)
+
+		// Now that we have the post data (credentials being tried by the user)
+		// ...we can check them against the database!
+		const userPOST = request.postdata["user"]
+		const pswdPOST = request.postdata["pswd"]
+
+		try {
+			const dbPromise = await Promise.resolve()
+				.then(() => sqlite.open('./database.sqlite', {
+					Promise
+				}))
+				.then(async (db) => {
+					// See if username is already taken
+					const [userRow] = await Promise.all([
+						db.get('SELECT * FROM Users WHERE Username = ?', userPOST)
+					]);
+					console.log("Accessing: \n", userRow);
+					if (userRow == undefined) { // Username not taken - lets make this account and aassign a cookie
+
+						// Generate cookie as a 10 digit number
+						// Something like base64 would be better but whatever its been 30+ hours of coding
+						cookie = (Math.round(Math.random() * 10000000000)).toString()
+
+						// Insert into the database the new row
+						const [beforeInsert, insertRow] = await Promise.all([
+							db.get('SELECT Username,Cookies FROM Users WHERE Username = ? AND Password = ?', userPOST, pswdPOST),
+							db.get('INSERT INTO Users (Username, Password, Cookies) VALUES (?,?,?)', userPOST, pswdPOST, cookie)
+						]);
+
+						// Grab the row of that user again to see the difference
+						const [afterInsert] = await Promise.all([
+							db.get('SELECT * FROM Users WHERE Username = ? AND Password = ?', userPOST, pswdPOST)
+						]);
+						console.log(beforeInsert, afterInsert);
+						if (afterInsert == undefined) {
+							console.error("Undefined row, could not insert"); // Problem!
+							response.end("There was a problem registering, try again.")
+						} else {
+							// Update their sessiontoken cookie, ignoring current cookie
+							setCookie(request, response, cookie);
+
+							console.log("Database user row inserted:");
+							console.log(afterInsert);
+							response.end("<h2>Your data from the db is: " + JSON.stringify(afterInsert) + '</h2><br>Loading chat...' + '<meta http-equiv="refresh" content="0; url=/chat">')
+						}
+					} else {
+						// User entered an existing username.
+						response.end('<h1>Username taken, try again!</h1>' +
+							'<meta http-equiv="refresh" content="2; url=/register">')
+					}
+				}).catch((error) => {
+					throw Error("Oopsie woopsie i couldnt check database, error is => \n" + error);
+				});
+
+		} catch (error) {
+			console.log("We made a little fucksie wucksie:\n" + error);
+			response.send("We made a little fucksie wucksie:\n" + error);
+		}
+	})
+
+})
 // Routing the url at path '/login' when user submits a form
 app.post('/login', async function (request, response) {
 	response.set('Content-Type', 'text/html');
@@ -148,7 +235,7 @@ app.post('/login', async function (request, response) {
 					if (userRow == undefined) { // User did not enter user/pswd creds that exist in db.
 						// Redirect back to GET.
 						response.end('<h1>Incorrect credentials, try again!</h1>' +
-							'<meta http-equiv="refresh" content="3; url=/login">')
+							'<meta http-equiv="refresh" content="2; url=/login">')
 					} else { // User entered valid creds.
 
 						// Generate cookie as a 10 digit number
@@ -174,7 +261,7 @@ app.post('/login', async function (request, response) {
 						} else {
 							console.log("Database user row updated:");
 							console.log(afterUpdate);
-							response.end("<h2>Your data from the db is: " + JSON.stringify(userRow) + '</h2><br>Loading chat...' + '<meta http-equiv="refresh" content="2; url=/chat">')
+							response.end("<h2>Your data from the db is: " + JSON.stringify(userRow) + '</h2><br>Loading chat...' + '<meta http-equiv="refresh" content="0; url=/chat">')
 						}
 					}
 				}).catch((error) => {
