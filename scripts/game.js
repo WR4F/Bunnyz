77a3f0976c2844ae9c041fa7b87ccbbc34d96b40
Chelsea Pierre <cpier114@fiu.edu>
2019-03-03 00:21:57 -0500

    Added database and user data

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,7 @@
 var socket = io();
 
 var count = 0;
-
+//gets data and pushes out that message
 socket.on('logthis', function (data) {
 	count++;
 	console.log("New message: #" + count + ", Data: " + data);
