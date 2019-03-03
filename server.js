6e63d9d2ef919b80497c940b2838eda34129ceeb
Jessela26 <jelbaniqued26@gmail.com>
2019-03-02 20:43:28 -0500

    changed html, added post in server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var socketIO = require('socket.io');
-var sqlite =  require('sqlite');
-
+var sqlite = require('sqlite');
 
 // Using libraries to create obejcts for communication
 var app = express();
@@ -13,11 +12,22 @@ var io = socketIO(server);
 
 // Set the web server port to 80
 app.set('port', 80);
+app.use(express.json())
 app.use('/scripts', express.static(__dirname + '/scripts'));
 
 // Routing the url at path '/' to the index.html file in the html folder
 app.get('/', function (request, response) {
 	response.sendFile(path.join(__dirname, 'html/index.html'));
+});
+app.get('/login', function (request, response) {
+	response.sendFile(path.join(__dirname, 'html/login.html'));
+});
+// Routing the url at path '/login' when user submits a form
+app.post('/login', function (request, response) {
+	// response.sendFile(path.join(__dirname, 'html/login.html'));
+	response.send("Thanks for your data my dog!\n")
+	console.log(request.query, request.body)
+	response.end(request.body)
 	
 });
 
@@ -26,13 +36,13 @@ app.get('/db', function (request, response) {
 
 	try {
 		const dbPromise = Promise.resolve()
-		.then(() => sqlite.open('./database.sqlite', { Promise }))
-		.then((db) => {
-			// interact with the database somehow
-			response.end("Some shit from the db: \n");
-		}).catch(() => {
-			throw Error("Oopsie woopsie i couldnt open the database and get my data properly so sorry senpai");
-		});
+			.then(() => sqlite.open('./database.sqlite', { Promise }))
+			.then((db) => {
+				// interact with the database somehow
+				response.end("Some shit from the db: \n");
+			}).catch(() => {
+				throw Error("Oopsie woopsie i couldnt open the database and get my data properly so sorry senpai");
+			});
 
 	} catch (error) {
 		console.log("We made a little fucksie wucksie:\n" + error);
@@ -51,7 +61,7 @@ server.listen(80, function () {
 
 
 // Starts the WebSocket server
-io.on('connection', function (socket) {});
+io.on('connection', function (socket) { });
 
 // Every 2000 seconds, send "hi!" to people connected
 setInterval(function () {
